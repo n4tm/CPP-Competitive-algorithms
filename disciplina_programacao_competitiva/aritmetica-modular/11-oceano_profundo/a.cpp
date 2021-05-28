324706c52e99b23d43b2878aaef1db521540b340
n4tm <nldm.eng20@uea.edu.br>
2021-05-28 11:45:34 -0400

    update

---
+++ disciplina_programacao_competitiva/aritmetica-modular/11-oceano_profundo/a.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+#define vec vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define MOD (int) 1e9+7
+int inv_mod(int a, int b){return a > 1 ? b-inv_mod(b%a, a)*b/a : 1;};
+
+string s;
+const int M = 131071;
+
+void solve() {
+	while(getline(cin, s, '#')) {
+		int n = 0;
+		int pow2 = 1;
+		for(int i=sz(s)-1; i>=0; i--) {
+			n += (((s[i] - '0')%M)*(pow2%M))%M;
+			pow2 = ((pow2%M) * (2%M))%M;
+		}
+		if(!n%M) cout << "YES";
+		else cout << "NO";
+		cout << '\n';
+	}
+
+};
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	solve();
+
+	return 0;
+}
