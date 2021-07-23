ceda8a86eae448872185d3439844afafef163d95
Natanael Lucena <nldm.eng20@uea.edu.br>
2021-07-23 13:02:44 -0400

    update

---
+++ disciplina_programacao_competitiva/03-busca-binaria/06-soma_das_casas/a.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+#define vec vector
+#define pb push_back
+#define ins insert
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define readintvec(v) for(int &x: v) scanf("%d", &x)
+#define MOD (int)(1e9+7)
+int inv_mod(int a, int b){return(a>1?b-inv_mod(b%a,a)*b/a:1);}
+unsigned int factorial(unsigned int n){int ans=1,i;for(i=2;i<=n;i++)ans*=i;return ans;}
+
+int b_search(vector<int> &v, int x, int k) {
+	int l = 0, r = sz(v), m;
+	while (l < r) {
+		m = l + (r-l)/2;
+		if (v[m]+x < k) l = m+1;
+		else r = m;
+	}
+	if (l >= sz(v) || v[l]+x != k) return -1;
+	return v[l];
+}
+
+void solve() {
+	int n, k;
+	scanf("%d", &n);
+	vector<int> v(n); for (int &x: v) scanf("%d", &x);
+	scanf("%d", &k);
+	for (int i = 0; i < n; i++) {
+		int result = b_search(v, v[i], k);
+		if (result == -1) continue;
+		else {
+			printf("%d %d\n", v[i], result);
+			break;
+		}
+	}
+};
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	solve();
+
+	return 0;
+}
